53cdf1265f9ec81bbc7e9382a12d35985c3a36d1
Krzysztof <k.d4browski@gmail.com>
2025-05-17 12:29:54 +0200

    Add seeders

--- PO2-projekt/Data/LibraryDbContext.cs
+++ PO2-projekt/Data/LibraryDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PO2_projekt.Models;
+using System;
 
 namespace PO2_projekt.Data
 {
@@ -20,6 +21,37 @@ namespace PO2_projekt.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            // SEED: Kategorie
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Poezja" },
+                new Category { Id = 2, Name = "Powieść" },
+                new Category { Id = 3, Name = "Fantastyka" }
+            );
+
+            // SEED: Autorzy
+            modelBuilder.Entity<Author>().HasData(
+                new Author { Id = 1, FirstName = "Adam", LastName = "Mickiewicz" },
+                new Author { Id = 2, FirstName = "Henryk", LastName = "Sienkiewicz" }
+            );
+
+            // SEED: Książki
+            modelBuilder.Entity<Book>().HasData(
+                new Book { Id = 1, Title = "Pan Tadeusz", YearPublished = 1834, Copies = 5, CategoryId = 1 },
+                new Book { Id = 2, Title = "Quo Vadis", YearPublished = 1896, Copies = 3, CategoryId = 2 }
+            );
+
+            // SEED: Relacja BookAuthor
+            modelBuilder.Entity<BookAuthor>().HasData(
+                new BookAuthor { BookId = 1, AuthorId = 1 },
+                new BookAuthor { BookId = 2, AuthorId = 2 }
+            );
+
+            // SEED: Czytelnicy
+            modelBuilder.Entity<Reader>().HasData(
+                new Reader { Id = 1, FirstName = "Jan", LastName = "Kowalski", Email = "jan.kowalski@example.com", Country = "Polska", CreateAt = new DateTime(2023,1,1) },
+                new Reader { Id = 2, FirstName = "Anna", LastName = "Nowak", Email = "anna.nowak@example.com", Country = "Polska", CreateAt = new DateTime(2023,2,1) }
+            );
 
             modelBuilder.Entity<BookAuthor>()
                 .HasKey(ba => new { ba.BookId, ba.AuthorId });
---
+++ PO2-projekt/Migrations/20250517102333_SeedInitialData.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PO2projekt.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "Adam", "Mickiewicz" },
+                    { 2, "Henryk", "Sienkiewicz" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Poezja" },
+                    { 2, "Powieść" },
+                    { 3, "Fantastyka" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Readers",
+                columns: new[] { "Id", "Country", "CreateAt", "Email", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "Polska", DateTime.SpecifyKind(new DateTime(2023, 1, 1), DateTimeKind.Utc), "jan.kowalski@example.com", "Jan", "Kowalski" },
+                    { 2, "Polska", DateTime.SpecifyKind(new DateTime(2023, 2, 1), DateTimeKind.Utc), "anna.nowak@example.com", "Anna", "Nowak" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "CategoryId", "Copies", "Title", "YearPublished" },
+                values: new object[,]
+                {
+                    { 1, 1, 5, "Pan Tadeusz", 1834 },
+                    { 2, 2, 3, "Quo Vadis", 1896 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "BookAuthors",
+                columns: new[] { "AuthorId", "BookId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "BookAuthors",
+                keyColumns: new[] { "AuthorId", "BookId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "BookAuthors",
+                keyColumns: new[] { "AuthorId", "BookId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Readers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Readers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
